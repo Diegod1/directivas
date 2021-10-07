fc936054da649cf992d22fd7e3f127eb51b58830
Diego Delgado <diegoad19@hotmail.com>
2021-10-07 14:29:43 -0500

    nuevo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,29 @@ import { Producto } from './producto.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'directivas';
-  codigoInput:string = '';
-  descripcionInput:string ='';
-  precioInput:string = '';
+  titulo = 'Administración de prodúctos';
+  codigoInput: string = '';
+  descripcionInput: string = '';
+  precioInput: string = '';
 
-  productos:Producto[] = [new Producto('AC989080','Papas margarita 34g','2100'),
-  new Producto('AC889080','Arroz Marina 1k','1800'),
-  new Producto('AC987780', 'Choco ramo 230 g','3000') ];
+  productos: Producto[] = [
+    new Producto('AC989080', 'Papas margarita 34g', '2100'),
+    new Producto('AC889080', 'Arroz Marina 1k', '1800'),
+    new Producto('AC987780', 'Choco ramo 230 g', '3000'),
+  ];
 
-agregarProducto(){
-  let Producto1 = new Producto(this.codigoInput, this.descripcionInput,this.precioInput);
-  this.productos.push(Producto1);
-}
-borrarProducto(){
-  this.productos.pop();
-}
+  agregarProducto() {
+    let Producto1 = new Producto(
+      this.codigoInput,
+      this.descripcionInput,
+      this.precioInput
+    );
+    this.productos.push(Producto1);
+  }
+  borrarProducto() {
+    this.productos.pop();
+  }
 }
